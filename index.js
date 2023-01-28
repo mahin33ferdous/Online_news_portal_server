7f3ee3fb6474952cd0eae57d7f72b1581829e273
asad <asaduzzaman@lapasar.com>
2023-01-29 03:44:54 +0600

    new routing added

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors=require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+const categories=require('./data/category.json');
+const news=require('./data/news.json');
+app.get('/',(req,res)=>{
+    res.send('news api');
+});
+
+app.get('/category',(req,res)=>{
+    res.send(categories);
+});
+
+app.get('/category/:id',(req,res)=>{
+   const id= req.params.id; //requested id from url;
+   if(id==='08'){
+    res.send(news);
+   } 
+   else{
+    const category_news=news.filter(n=>n.category_id===id)
+    res.send(category_news);
+    console.log(category_news)
+   }
+  
+
+});
+app.get('/news',(req,res)=>{
+    res.send(news);
+})
+app.get('/news/:id',(req,res)=>{
+
+    const id=req.params.id;
+    const selectedNews=news.find(n=>n._id===id);
+    res.send(selectedNews);
+
+});
+
+app.listen(port,()=>{
+    console.log('nes server')
+})
